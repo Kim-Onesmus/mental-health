be0c89b83b5ab3e5e4bf2d5fe2c29ca05973eea5
Kim-Onesmus <kimones1122@gmail.com>
2025-08-11 11:56:54 +0300

    updates

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Facility
 
+class FacilityResource(resources.ModelResource):
+    class Meta:
+        model = Facility
+
 @admin.register(Facility)
-class FacilityAdmin(admin.ModelAdmin):
+class FacilityAdmin(ImportExportModelAdmin):
+    resource_class = FacilityResource
     list_display = ('name', 'type', 'county', 'subcounty', 'constituency', 'ward')
     list_filter = ('type', 'county', 'subcounty')
     search_fields = ('name', 'county', 'subcounty', 'constituency', 'ward')
