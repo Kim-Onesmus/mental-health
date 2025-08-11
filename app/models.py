cd05cf2136aa600da94696f69653e51c5bc28e6b
Kim-Onesmus <kimones1122@gmail.com>
2025-08-11 11:34:34 +0300

    updates

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Facility(models.Model):
+    FACILITY_TYPES = [
+        ('mental', 'Mental Health'),
+        ('rehab', 'Rehabilitation'),
+    ]
+
+    name = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    type = models.CharField(max_length=10, choices=FACILITY_TYPES)
+    county = models.CharField(max_length=50)
+    subcounty = models.CharField(max_length=50)
+    constituency = models.CharField(max_length=50)
+    ward = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
